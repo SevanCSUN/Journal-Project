8640767426c7de6f4c38f2163793c78b8590b0e2
Daniel Gilberto <daniel.gilberto.252@yahoo.com>
2024-11-18 14:58:52 -0800

    Editted indiv_page_view to implement fleather

--- journal_project/lib/src/sample_feature/indiv_page_view.dart
+++ journal_project/lib/src/sample_feature/indiv_page_view.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:fleather/fleather.dart';
 import 'package:flutter/material.dart';
 
 class IndivPageView extends StatefulWidget {
@@ -15,34 +18,15 @@ class IndivPageView extends StatefulWidget {
 }
 
 class _IndivPageViewState extends State<IndivPageView> {
-  TextEditingController _controller = TextEditingController();
-  bool _isBold = false;
-  bool _isItalic = false;
-  bool _isUnderline = false;
+  late ParchmentDocument document;
+  late FleatherController controller;
 
   @override
   void initState() {
     super.initState();
-    _controller.addListener(_updateText);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _updateText() {
-    setState(() {}); // Update the UI whenever the text changes
-  }
-
-  TextStyle getCurrentStyle() {
-    return TextStyle(
-      fontWeight: _isBold ? FontWeight.bold : FontWeight.normal,
-      fontStyle: _isItalic ? FontStyle.italic : FontStyle.normal,
-      decoration:
-          _isUnderline ? TextDecoration.underline : TextDecoration.none,
-    );
+    // Initialize with empty document or define a valid JSON map
+    document = ParchmentDocument();
+    controller = FleatherController(document: document);
   }
 
   @override
@@ -50,78 +34,19 @@ class _IndivPageViewState extends State<IndivPageView> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.pageTitle),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.format_bold,
-              color: _isBold ? Colors.blue : Colors.black,
-            ),
-            onPressed: () {
-              setState(() {
-                _isBold = !_isBold;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.format_italic,
-              color: _isItalic ? Colors.blue : Colors.black,
-            ),
-            onPressed: () {
-              setState(() {
-                _isItalic = !_isItalic;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.format_underline,
-              color: _isUnderline ? Colors.blue : Colors.black,
-            ),
-            onPressed: () {
-              setState(() {
-                _isUnderline = !_isUnderline;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.clear),
-            onPressed: () {
-              _controller.clear();
-            },
-          ),
-        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Stack(
-          children: [
-            if (_controller.text.isEmpty) // Display hint text when input is empty
-              Positioned(
-                top: 0,
-                left: 0,
-                child: Text(
-                  'Start your journal here...',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-            EditableText(
-              controller: _controller,
-              focusNode: FocusNode(),
-              style: getCurrentStyle(),
-              cursorColor: Colors.blue,
-              backgroundCursorColor: Colors.grey,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              selectionControls: MaterialTextSelectionControls(),
-              textAlign: TextAlign.start,
-              selectionColor: Colors.blue.withOpacity(0.3),
+      body: Column(
+        children: [
+          FleatherToolbar.basic(controller: controller),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0), // Add padding as needed
+              child: FleatherEditor(controller: controller),
             ),
-          ],
-        ),
+          ),
+          // Alternatively, use FleatherField if needed
+          // FleatherField(controller: controller)
+        ],
       ),
     );
   }
